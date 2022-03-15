03f9ef83a1d9a2b61b87b67fd2f8c658468d4449
america <villalobosamericacbtis183@gmail.com>
2022-03-15 11:54:53 -0600

    tabla de datos primitivos

---
+++ src/principal/Principal.java
@@ -0,0 +1,46 @@
+package principal;
+
+public class Principal {
+	
+	public static void main(String []args) {
+		//primitivos
+		
+	    System.out.println("************************************************");
+	    System.out.println("*****Nombre**********************longuitud**************************************ejemplo**********");
+	    System.out.println("*datos primitivos  *                                                 * ");
+		int numero;
+		numero = 20;
+		System.out.println("*        int       *         -2147483648 a 2147483647                *        "+ numero);
+		
+	     float num;
+	     num = 21;
+	     System.out.println("*       float      *       +-3.4x(10*(-38)) a +-3.4x(10(38))         *        "+ num);
+	     
+	     short num1;
+	     num1 = 45;
+	     System.out.println("*        short     *       -32768 a 32767                            *         "+ num1);
+	     
+	     byte num2;
+	     num2 = 10;
+	     System.out.println("*        byte      *            -128 + 127                           *         "+ num2);
+	     
+	     double num3;
+	     num3 = 56;
+	     System.out.println("*       double     *        1.8x(10(-308)) a -+ 1.8x(10(308))        *        "+ num3);
+	     
+	     boolean num4;
+	     num4 = true;
+	     System.out.println("*       boolean    *          true o false                           *        "+ num4);
+	     System.out.println("*                  *                                                 *");
+	     char alum;
+	     alum = 97;
+	     System.out.println("*         char     *          u0000 a uFFFF                          *         "+alum);
+	     System.out.println("*datos de objetos  *                    *                            *");
+	     
+	   
+	     
+	      
+	     
+	}
+
+}
